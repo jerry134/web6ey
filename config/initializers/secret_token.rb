ba7bc69cc47eedf38c3479fd655b0ddb7d4f84ee
jerry134 <117064092@qq.com>
2013-04-16 15:28:10 +0800

    init rails project with mysql database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web6bey::Application.config.secret_token = '9d0df4c2619d17f6f239a5460d27a981571a16a0d76b2570548e9b9464ad2d1b22e1bd31896dae69f9839b1570180c9ca19a9ca33aba47ec8258c6779de6cad8'
