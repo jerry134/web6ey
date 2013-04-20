13284d3c0177e1bd692426b4078f96286d24a94e
Ken Lu <ken@seeice.com>
2013-04-20 15:30:50 -0400

    added some comment to model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text             default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Question < ActiveRecord::Base
 
   validates_presence_of :title, :content
