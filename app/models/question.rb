36f0a51f2dc65a0ff59c2b6985ff819d2a1d74b9
Ken Lu <ken@seeice.com>
2013-04-20 16:18:13 -0400

    display username of the question

--- app/models/question.rb
+++ app/models/question.rb
@@ -16,6 +16,6 @@ class Question < ActiveRecord::Base
   attr_accessible :content, :title, :tag_list
   acts_as_taggable
   belongs_to :user
-  delegate :email, to: :user, allow_nil: true, prefix: 'owner'
+  delegate :username, to: :user, allow_nil: true, prefix: 'owner'
   scope :owner, joins(:user)
 end
