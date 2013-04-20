7877039fcb7bbde43a59c778df8179e4f7b28063
Ken Lu <ken@seeice.com>
2013-04-20 15:40:01 -0400

    added the support of username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130420184938) do
+ActiveRecord::Schema.define(:version => 20130420193356) do
 
   create_table "questions", :force => true do |t|
     t.string   "title"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20130420184938) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
