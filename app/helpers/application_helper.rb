ef926581eb698134c82f6060ad3e397d7ca5a713
songjiayang <songjiayang1@gmail.com>
2013-04-22 23:43:10 +0800

    fiexed question css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+#encoding:utf-8
 module ApplicationHelper
+
+	def render_status question
+		if question.answers.blank?
+			raw (link_to question.user.username)+"于#{(Time.now-question.created_at.localtime).to_i/60}分钟前发布"
+		else
+			raw (link_to question.answers.last.user.username)+"于#{(Time.now-question.answers.last.created_at.localtime).to_i/60}分钟前回复"
+		end
+	end
 end
