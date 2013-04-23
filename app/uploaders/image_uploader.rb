df49324ec9dad065f4fe90b659150f5643a49bc8
Ken Lu <ken@seeice.com>
2013-04-22 21:26:05 -0400

    Fixed the error production mode

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,6 +10,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+
+  def default_url
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
